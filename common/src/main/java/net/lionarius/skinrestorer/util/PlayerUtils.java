b72d0c1e85645bae9291f13c1dcdc0782397f7ff
lionarius <main@lionarius.ru>
2024-08-03 17:40:12 +0300

    backport to 1.20.5-1.20.6

--- common/src/main/java/net/lionarius/skinrestorer/util/PlayerUtils.java
+++ common/src/main/java/net/lionarius/skinrestorer/util/PlayerUtils.java
@@ -85,7 +85,7 @@ public final class PlayerUtils {
             playerList.sendPlayerPermissionLevel(player);
             playerList.sendLevelInfo(player, serverLevel);
             playerList.sendAllPlayerInfo(player);
-            playerList.sendActivePlayerEffects(player);
+            PlayerUtils.sendActivePlayerEffects(player);
         }
     }
     
