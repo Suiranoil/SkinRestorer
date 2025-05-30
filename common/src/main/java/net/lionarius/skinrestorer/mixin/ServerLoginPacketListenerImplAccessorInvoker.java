56655bca36697b3b30fd1e6c1e93d240c9ccfcce
Lionarius <git@lionarius.ru>
2025-05-30 22:39:37 +0300

    backport to 1.20

--- common/src/main/java/net/lionarius/skinrestorer/mixin/ServerLoginPacketListenerImplAccessorInvoker.java
+++ common/src/main/java/net/lionarius/skinrestorer/mixin/ServerLoginPacketListenerImplAccessorInvoker.java
@@ -4,10 +4,14 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ServerLoginPacketListenerImpl.class)
 public interface ServerLoginPacketListenerImplAccessorInvoker {
     
     @Accessor
-    GameProfile getAuthenticatedProfile();
+    GameProfile getGameProfile();
+    
+    @Invoker
+    GameProfile invokeCreateFakeProfile(GameProfile original);
 }
