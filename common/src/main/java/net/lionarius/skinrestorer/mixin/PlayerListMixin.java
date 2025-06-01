757d46b231c5c61ef9d149df10470c45915123be
Lionarius <git@lionarius.ru>
2025-06-01 18:24:23 +0300

    Revert "remove shadow of server from PlayerListMixin"
    This reverts commit 8119a08c80ae3536e34d3ebc12f1953b235bab47.

--- common/src/main/java/net/lionarius/skinrestorer/mixin/PlayerListMixin.java
+++ common/src/main/java/net/lionarius/skinrestorer/mixin/PlayerListMixin.java
@@ -6,16 +6,23 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
+import java.util.List;
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
+    
+    @Shadow @Final
+    private MinecraftServer server;
+    
     @Inject(method = "remove", at = @At("TAIL"))
     private void remove(ServerPlayer player, CallbackInfo ci) {
         SkinRestorer.Events.onPlayerDisconnect(player);
@@ -30,7 +37,6 @@ public abstract class PlayerListMixin {
     
     @Inject(method = "placeNewPlayer", at = @At("HEAD"))
     private void placeNewPlayer(Connection connection, ServerPlayer player, CommonListenerCookie cookie, CallbackInfo ci) {
-        var server = ((PlayerList) (Object) this).getServer();
         var delay = SkinRestorer.getConfig().skinApplyDelayOnJoin();
         
         if (delay <= 0) {
