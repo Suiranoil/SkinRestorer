0524b5dab8e3792f39c0cd6c882e76f2f6ea5c57
Lionarius <git@lionarius.ru>
2026-01-14 23:39:53 +0300

    the worst bug I encountered in gson 2.10.1

--- common/src/main/java/net/lionarius/skinrestorer/config/provider/collection/CollectionSkinSourceListDeserializer.java
+++ common/src/main/java/net/lionarius/skinrestorer/config/provider/collection/CollectionSkinSourceListDeserializer.java
@@ -6,7 +6,12 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CollectionSkinSourceListDeserializer implements JsonDeserializer<List<CollectionSkinSource>> {
+public class CollectionSkinSourceListDeserializer implements JsonSerializer<List<CollectionSkinSource>>, JsonDeserializer<List<CollectionSkinSource>> {
+    @Override
+    public JsonElement serialize(List<CollectionSkinSource> src, Type typeOfT, JsonSerializationContext context) {
+        return context.serialize(src, List.class);
+    }
+    
     @Override
     public List<CollectionSkinSource> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
