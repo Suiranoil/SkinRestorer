0a097285326eb340c358abbbd4406ae6506bfde3
Lionarius <git@lionarius.ru>
2026-02-19 20:39:24 +0300

    add custom username url skin provider

--- common/src/main/java/net/lionarius/skinrestorer/config/provider/custom/CustomProviderListDeserializer.java
+++ common/src/main/java/net/lionarius/skinrestorer/config/provider/custom/CustomProviderListDeserializer.java
@@ -30,6 +30,7 @@ public final class CustomProviderListDeserializer implements JsonSerializer<List
             CustomProviderConfig provider = switch (providerType) {
                 case YGGDRASIL -> context.deserialize(object, CustomYggdrasilProviderConfig.class);
                 case AUTHLIB_INJECTOR -> context.deserialize(object, CustomAuthlibInjectorProviderConfig.class);
+                case USERNAME_URL -> context.deserialize(object, CustomUsernameUrlProviderConfig.class);
                 default -> context.deserialize(object, UnknownCustomProviderConfig.class);
             };
             
