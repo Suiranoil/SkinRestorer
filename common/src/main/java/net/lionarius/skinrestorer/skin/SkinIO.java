45df31392120b21e1d90e208cdf4c7c6773afc0d
Lionarius <git@lionarius.ru>
2026-03-09 23:46:31 +0300

    remove deprecated old skin file format migration

--- common/src/main/java/net/lionarius/skinrestorer/skin/SkinIO.java
+++ common/src/main/java/net/lionarius/skinrestorer/skin/SkinIO.java
@@ -1,14 +1,10 @@
 package net.lionarius.skinrestorer.skin;
 
-import com.mojang.authlib.properties.Property;
-import net.lionarius.skinrestorer.skin.provider.builtin.MineskinSkinProvider;
-import net.lionarius.skinrestorer.skin.provider.builtin.MojangSkinProvider;
 import net.lionarius.skinrestorer.util.FileUtils;
 import net.lionarius.skinrestorer.util.JsonUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.UUID;
 
 public class SkinIO {
@@ -38,8 +34,7 @@ public class SkinIO {
         try {
             return JsonUtils.fromJson(json, SkinValue.class);
         } catch (Exception e) {
-            var property = JsonUtils.fromJson(json, Property.class);
-            return SkinIO.convertFromOldFormat(property);
+            return SkinValue.EMPTY;
         }
     }
     
@@ -53,33 +48,5 @@ public class SkinIO {
     
     private static String uuidToFilename(UUID uuid) {
         return uuid + FILE_EXTENSION;
-    }
-    
-    private static SkinValue convertFromOldFormat(Property property) {
-        try {
-            var propertyJson = Objects.requireNonNull(JsonUtils.skinPropertyToJson(property));
-            var textures = propertyJson.getAsJsonObject("textures");
-            
-            var capeTexture = textures.getAsJsonObject("CAPE");
-            if (capeTexture != null) {
-                var profileName = propertyJson.get("profileName").getAsString();
-                return new SkinValue(MojangSkinProvider.PROVIDER_NAME, profileName, null, property);
-            }
-            
-            var skinTexture = textures.getAsJsonObject("SKIN");
-            var url = skinTexture.get("url").getAsString();
-            
-            var variant = SkinVariant.CLASSIC;
-            var metadata = skinTexture.getAsJsonObject("metadata");
-            if (metadata != null) {
-                var model = metadata.get("model");
-                if (model != null && "slim".equals(model.getAsString()))
-                    variant = SkinVariant.SLIM;
-            }
-            
-            return new SkinValue(MineskinSkinProvider.PROVIDER_NAME, url, variant, property);
-        } catch (Exception e) {
-            return SkinValue.EMPTY;
-        }
     }
 }
