2918f93c785394ef96b5c716b7745c37106f06f9
Lionarius <git@lionarius.ru>
2026-03-17 22:55:55 +0300

    reformat

--- common/src/main/java/net/lionarius/skinrestorer/mixin/SkullBlockEntityMixin.java
+++ common/src/main/java/net/lionarius/skinrestorer/mixin/SkullBlockEntityMixin.java
@@ -4,7 +4,6 @@ import com.mojang.authlib.GameProfile;
 import net.lionarius.skinrestorer.SkinRestorer;
 import net.lionarius.skinrestorer.util.PlayerUtils;
 import net.minecraft.Util;
-import net.minecraft.server.Services;
 import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.world.level.block.entity.SkullBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.BooleanSupplier;
 
 @Mixin(SkullBlockEntity.class)
 public abstract class SkullBlockEntityMixin {
@@ -25,15 +23,16 @@ public abstract class SkullBlockEntityMixin {
     @Shadow
     private static GameProfileCache profileCache;
 
-    @Inject(method = "fetchGameProfile", at = @At("HEAD"),
-            cancellable = true)
-    private static void fetchProfileByName(String name, CallbackInfoReturnable<CompletableFuture<Optional<GameProfile>>> cir) {
+    @Inject(method = "fetchGameProfile", at = @At("HEAD"), cancellable = true)
+    private static void fetchProfileByName(
+            String name, CallbackInfoReturnable<CompletableFuture<Optional<GameProfile>>> cir) {
         if (profileCache == null) return;
 
         if (name == null) return;
 
         var profileOpt = Optional.<GameProfile>empty();
-        var gameProfileInfo = ((GameProfileCacheAccessor) profileCache).getProfilesByName().get(name.toLowerCase(Locale.ROOT));
+        var gameProfileInfo =
+                ((GameProfileCacheAccessor) profileCache).getProfilesByName().get(name.toLowerCase(Locale.ROOT));
 
         if (gameProfileInfo != null) profileOpt = Optional.of(gameProfileInfo.getProfile());
 
--- common/src/main/java/net/lionarius/skinrestorer/util/PlayerUtils.java
+++ common/src/main/java/net/lionarius/skinrestorer/util/PlayerUtils.java
@@ -66,7 +66,8 @@ public final class PlayerUtils {
         }
 
         if (!player.isDeadOrDying()) {
-            player.connection.send(new ClientboundRespawnPacket(player.createCommonSpawnInfo(serverLevel), ClientboundRespawnPacket.KEEP_ALL_DATA));
+            player.connection.send(new ClientboundRespawnPacket(
+                    player.createCommonSpawnInfo(serverLevel), ClientboundRespawnPacket.KEEP_ALL_DATA));
             player.connection.teleport(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
             player.connection.send(new ClientboundSetEntityMotionPacket(player));
             var vehicle = player.getVehicle();
