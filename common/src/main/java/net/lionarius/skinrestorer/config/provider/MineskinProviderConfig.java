45300594cd908f70f662285061922d2ab2a2dd55
Lionarius <git@lionarius.ru>
2026-07-20 01:17:02 +0300

    add allowed domains config option for web skin provider

--- common/src/main/java/net/lionarius/skinrestorer/config/provider/MineskinProviderConfig.java
+++ common/src/main/java/net/lionarius/skinrestorer/config/provider/MineskinProviderConfig.java
@@ -3,17 +3,22 @@ package net.lionarius.skinrestorer.config.provider;
 import net.lionarius.skinrestorer.SkinRestorer;
 import net.lionarius.skinrestorer.skin.provider.builtin.MineskinSkinProvider;
 
+import java.util.List;
+import java.util.Locale;
+
 public final class MineskinProviderConfig extends BuiltInProviderConfig {
     private static final CacheConfig DEFAULT_CACHE_VALUE = new CacheConfig(true, 300);
 
     private String apiKey;
     private boolean proxyUrlUpload;
+    private List<String> allowedDomains;
 
     public MineskinProviderConfig() {
         super(MineskinSkinProvider.PROVIDER_NAME, DEFAULT_CACHE_VALUE);
 
         this.apiKey = "";
         this.proxyUrlUpload = false;
+        this.allowedDomains = List.of();
     }
 
     public String apiKey() {
@@ -24,6 +29,10 @@ public final class MineskinProviderConfig extends BuiltInProviderConfig {
         return this.proxyUrlUpload;
     }
 
+    public List<String> allowedDomains() {
+        return this.allowedDomains;
+    }
+
     @Override
     public void gsonPostProcess() {
         super.validate(MineskinSkinProvider.PROVIDER_NAME, DEFAULT_CACHE_VALUE);
@@ -31,6 +40,15 @@ public final class MineskinProviderConfig extends BuiltInProviderConfig {
         if (this.apiKey == null) {
             SkinRestorer.LOGGER.warn("Mineskin API key is null, defaulting to an empty string");
             this.apiKey = "";
+        }
+
+        if (this.allowedDomains == null) {
+            this.allowedDomains = List.of();
+        } else {
+            this.allowedDomains = this.allowedDomains.stream()
+                    .filter(domain -> domain != null && !domain.isBlank())
+                    .map(domain -> domain.trim().toLowerCase(Locale.ROOT))
+                    .toList();
         }
     }
 }
